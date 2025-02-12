6813b283c4f29f94b30d8277f972b6518fba34b1
Masonwp123 <masonparker868@gmail.com>
2025-02-12 15:49:26 +0000

    initial Code commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
+
+	//open file
+	std::ifstream inFile;
+	inFile.open("data.csv");
+
+	//setup ss and converter for lines
+	std::stringstream ss;
+	std::stringstream converter;
+
+	std::string item;
+	while (getline(inFile, item)) {
+
+		//variables
+		int firstVal;
+		std::string sFirstVal;
+		int secondVal;
+		std::string sSecondVal;
+		std::string text;
+
+		//clear ss and set new item
+		ss.clear();
+		ss.str(item);
+
+		//get line and use comma as delimiter
+		getline(ss, sFirstVal, ',');
+		getline(ss, sSecondVal, ',');
+		getline(ss, text);
+
+		//clear converter and convert first val
+		converter.clear();
+		converter.str("");
+
+		converter << sFirstVal;
+		converter >> firstVal;
+
+		//clear converter and convert second val
+		converter.clear();
+		converter.str("");
+
+		converter << sSecondVal;
+		converter >> secondVal;
+
+		int total = firstVal + secondVal;
+		for (int i = 0; i < total; ++i) {
+			std::cout << text;
+		}
+		std::cout << std::endl;
+	}	
+
 	return 0;
 }
